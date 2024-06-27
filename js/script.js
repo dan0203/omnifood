e87ff8dca385912d4690ee041b2729fef888f634
Dan <danzerbib@gmail.com>
2024-06-27 10:49:17 +0200

    Toggling mobile nav with js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
+// Keep current year correct in copyright
 const yearEl = document.querySelector('.year');
 const currentYear = new Date().getFullYear();
-
 yearEl.textContent = currentYear;
+
+// Mobile navigation toggle
+const headerEl = document.querySelector('.header');
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+btnNavEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open');
+});
